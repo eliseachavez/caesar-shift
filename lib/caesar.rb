6d4617bab165c9f40fa25e522128afe8f73bdeb6
lemur <noctiphobic@gmail.com>
2022-08-26 12:47:11 -0500

    Wrap everything into a Cipher class

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -31,76 +31,75 @@
 @letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 # METHODS #################
-
-def get_shift
-  puts "Enter the shift"
-  shift = gets.chomp.to_i
-  if shift.is_a?(Integer)
-    shift
-  else
-    "That was not a valid shift"
+class Cipher
+  def initialize
+    @msg = get_message
+    @shift = get_shift
   end
-end
 
-def get_message()
-  puts "Enter a message"
-  message = gets.chomp
-end
-
-def encrypt(mesg_arr, shift)
-  mesg_arr.map do |item|
-    if @letters.include?(item)
-      if item.upcase == item
-        item = item.downcase
-        num_mesg = to_num(item)
-        shifted_mesg = shift_num(num_mesg, shift)
-        encr_mesg = to_str(shifted_mesg)
-        encr_mesg = encr_mesg.upcase
-      else #it's lowercase
-        num_mesg = to_num(item)
-        shifted_mesg = shift_num(num_mesg, shift)
-        encr_mesg = to_str(shifted_mesg)
-      end
+  def get_shift
+    puts "Enter the shift"
+    shift = gets.chomp.to_i
+    if shift.is_a?(Integer)
+      shift
     else
-      item
+      "That was not a valid shift"
     end
   end
-end
 
-def to_num(item)
-  item = @alphabet[item.to_sym]
-end
-
-def shift_num(num_mesg, shift)
-  num_mesg = num_mesg - shift
-  if num_mesg < 0
-    num_mesg = 25 + num_mesg
-  else
-    num_mesg
+  def get_message()
+    puts "Enter a message"
+    message = gets.chomp
   end
-end
 
-def to_str(shifted_mesg)
-  new_alphabet = @alphabet.invert
-  new_mesg = new_alphabet[shifted_mesg]
-  new_mesg = new_mesg.to_s
+  def encrypt(mesg_arr, shift)
+    mesg_arr.map do |item|
+      if @letters.include?(item)
+        if item.upcase == item
+          item = item.downcase
+          num_mesg = to_num(item)
+          shifted_mesg = shift_num(num_mesg, shift)
+          encr_mesg = to_str(shifted_mesg)
+          encr_mesg = encr_mesg.upcase
+        else #it's lowercase
+          num_mesg = to_num(item)
+          shifted_mesg = shift_num(num_mesg, shift)
+          encr_mesg = to_str(shifted_mesg)
+        end
+      else
+        item
+      end
+    end
+  end
+
+  def convert_to_array(message)
+    message.split('')
+  end
+
+  def to_num(item)
+    item = @alphabet[item.to_sym]
+  end
+
+  def shift_num(num_mesg, shift)
+    num_mesg = num_mesg - shift
+    if num_mesg < 0
+      num_mesg = 25 + num_mesg
+    else
+      num_mesg
+    end
+  end
+
+  def to_str(shifted_mesg)
+    new_alphabet = @alphabet.invert
+    new_mesg = new_alphabet[shifted_mesg]
+    new_mesg = new_mesg.to_s
+  end
 end
 
 # MAIN ###########
-message = get_message
-shift = get_shift
-
-# Convert string to characters, characters to numbers
-message = convert_to_array(message)
-def convert_to_array(message)
-  message.split('')
-end
-
+caesar_cipher = Cipher.new
+mesg_arr = convert_to_array(message)
 encrypted_mesg = encrypt(mesg_arr, shift)
-encrypted_mesg = encrypted_mesg.join("")
+encrypted_mesg = encrypted_mesg.join('')
 p encrypted_mesg
-# is is punctuation? leave it alone?
-# is it uppercase? note that
-# look up number equivalency
-# subtract shift (wrap from 0 to 26 instead of going negative)
-# convert back to a letter using lookup
+
