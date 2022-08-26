3c05eaa3151db391db57ad88de06c7847bf6478f
lemur <noctiphobic@gmail.com>
2022-08-26 12:38:21 -0500

    Add test cases for checking that non alphabetic characters stay unencrypted

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -2,20 +2,36 @@
 
 require './lib/caesar'
 
-
 describe '#get_message' do
   it 'returns a lowercase message' do
-    expect(get_message('STRINGY MCSTRINGFACE')).to eql('stringy mcstringface'))
+    msg = get_message
+    expect(get_message).to eql(get_message.chomp)
   end
-
   it 'returns a lowercase message with non-alphabetic characters' do
     expect(get_message('NICE TO MEET YOU STRING, I\'M DAD')).to eql('nice to meet you string, i\'m dad')
   end
 end
 
+describe '#convert_to_array' do
+  it 'returns an array of characters' do
+    expect(convert_to_array('hi, friend!')).to eql(['h', 'i', ',', '', 'f', 'r', 'i', 'e', 'n', 'd', '!'])
+  end
+end
+
 # Is message punctuation left unencrypted?
-it 'returns a message that leaves punctuation unencrypted' do
-  expect(get_message('Wow, what a string!')).to eql('Btb, bmfy f xywnsl!')
+it 'returns an encrypted message where punctuation stays unencrypted' do
+  expect('i\'m dad, son.').to eql('m\'s ifi, xts.')
+end
+
+it 'returns an encrypted message where numbers stay unencrypted' do
+  expect('1 great mom').to eql('1 lwjfy rtr')
+end
+
+it 'returns an encrypted message where non-punctuation, non-numbers, and non-alphabetic characters are unencrypted' do
+  expect('#@mom$~').to eql('#@rtr$~')
+end
+# it 'returns a message that leaves punctuation unencrypted' do
+#   expect(get_message('Wow, what a string!')).to eql('Btb, bmfy f xywnsl!')
 
 # is the shift an integer? not a string
   # expect(get_shift('1').to eql(1))
