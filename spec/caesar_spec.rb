daa5a70bd520add403173bf8d54b84e502330142
lemur <noctiphobic@gmail.com>
2022-08-26 12:07:36 -0500

    Write test case that checks for lowercase string message even when non alphabetic characters are present

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -2,14 +2,20 @@
 
 require './lib/caesar'
 
-# is the message lowercase?
-describe '' do
-  it 'returns a message that is a lowercase string' do
-  expect(get_message('What a string').to eql('Bmfy f xywnsl'))
+
+describe '#get_message' do
+  it 'returns a lowercase message' do
+    expect(get_message('STRINGY MCSTRINGFACE')).to eql('stringy mcstringface'))
+  end
+
+  it 'returns a lowercase message with non-alphabetic characters' do
+    expect(get_message('NICE TO MEET YOU STRING, I\'M DAD')).to eql('nice to meet you string, i\'m dad')
   end
 end
 
-# is punctuation left unencrypted?
+# Is message punctuation left unencrypted?
+it 'returns a message that leaves punctuation unencrypted' do
+  expect(get_message('Wow, what a string!')).to eql('Btb, bmfy f xywnsl!')
 
 # is the shift an integer? not a string
   # expect(get_shift('1').to eql(1))
