24aba4e23669a8c862eee772b910e37738b1c85a
lemur <noctiphobic@gmail.com>
2022-08-26 15:27:20 -0500

    Add new functionality for all methods

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,102 +1,132 @@
 # frozen_string_literal: true
 
-@alphabet = {
-  a: 0,
-  b: 1,
-  c: 2,
-  d: 3,
-  e: 4,
-  f: 5,
-  g: 6,
-  h: 7,
-  i: 8,
-  j: 9,
-  k: 10,
-  l: 11,
-  m: 12,
-  n: 13,
-  o: 14,
-  p: 15,
-  q: 16,
-  r: 17,
-  s: 18,
-  t: 19,
-  u: 20,
-  v: 21,
-  w: 22,
-  x: 23,
-  y: 24,
-  z: 25
-}
-@letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-# METHODS #################
+# Class that takes a message and encrypts as a caesar cipher shift
 class Cipher
   attr_accessor :msg, :shift
+
   def initialize
+    @alphabet = {
+      a: 0,
+      b: 1,
+      c: 2,
+      d: 3,
+      e: 4,
+      f: 5,
+      g: 6,
+      h: 7,
+      i: 8,
+      j: 9,
+      k: 10,
+      l: 11,
+      m: 12,
+      n: 13,
+      o: 14,
+      p: 15,
+      q: 16,
+      r: 17,
+      s: 18,
+      t: 19,
+      u: 20,
+      v: 21,
+      w: 22,
+      x: 23,
+      y: 24,
+      z: 25
+    }
+    @numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    @encrpt_msg = nil
     begin_encryption_steps
   end
 
   def begin_encryption_steps
     get_message
     get_shift
-    convert_to_array
     parse
     encrypt
-    @msg.join('')
+    numbers_back_to_letters
     print_msg
   end
 
   def get_shift
-    puts "Enter the shift"
+    puts 'Enter the shift'
     @shift = gets.chomp.to_i
   end
 
-  def get_message()
-    puts "Enter a message"
-    @msg = gets.chomp
+  def get_message
+    puts 'Enter a message'
+    msg = gets.chomp
+    @encrpt_msg = msg
+    @msg = msg
   end
 
   def parse
-    @msg.downcase
+    correct_case
+    convert_all_to_numbers
   end
 
   def encrypt
-    mesg_arr.map do |char|
-      num_mesg = to_num(item)
-      shifted_mesg = shift_num(num_mesg, shift)
-      encr_mesg = to_str(shifted_mesg)
+    @encrpt_msg.map! do |char|
+      shift_char(char)
     end
   end
 
-  def convert_to_array
-    @msg.split('')
+  def correct_case
+    @encrpt_msg.downcase
   end
 
-  def to_num(item)
-    item = @alphabet[item.to_sym]
-  end
-
-  def shift_num(num_mesg, shift)
-    num_mesg = num_mesg - shift
-    if num_mesg < 0
-      num_mesg = 25 + num_mesg
-    else
-      num_mesg
+  def convert_all_to_numbers
+    temp_msg = @encrpt_msg.split('')
+    temp_msg.map! do |char|
+      if @alphabet.include?(char.to_sym)
+        @alphabet[char.to_sym]
+      else
+        char
+      end
     end
+    @encrpt_msg = temp_msg
   end
 
-  def to_str(shifted_mesg)
-    new_alphabet = @alphabet.invert
-    new_mesg = new_alphabet[shifted_mesg]
-    new_mesg = new_mesg.to_s
+  def shift_char(char)
+    if char.is_a?(Integer)
+      char = @shift >= 0 ? pos_shift(char) : neg_shift(char)
+    end
+    char
+  end
+
+  def pos_shift(char)
+    @shift %= 25 unless @shift.zero?
+    char += @shift
+    char
+  end
+
+  def neg_shift(char)
+    @shift.abs
+    @shift %= 25
+    @shift = -@shift
+    char += @shift
+    char
+  end
+
+  def numbers_back_to_letters
+    inverted_alphabet = @alphabet.invert
+    @msg = @msg.split('')
+    @msg.each_with_index do |char, idx|
+      @encrpt_msg.each_with_index do |e_char, e_idx|
+        next unless idx == e_idx
+
+        @encrpt_msg[idx] = (inverted_alphabet[e_char.to_i]).to_s if @letters.include?(char)
+      end
+    end
+    @encrpt_msg = @encrpt_msg.join('')
+    @msg = @msg.join('')
   end
 
   def print_msg
-    p @msg
+    puts "The original message was #{@msg} and the new encrypted message is #{@encrpt_msg}"
   end
 end
 
 # MAIN ###########
-caesar_cipher = Cipher.new
+Cipher.new
 
--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -2,34 +2,42 @@
 
 require './lib/caesar'
 
-describe '#get_message' do
-  it 'returns a lowercase message' do
-    msg = get_message
-    expect(get_message).to eql(get_message.chomp)
-  end
-  it 'returns a lowercase message with non-alphabetic characters' do
-    expect(get_message('NICE TO MEET YOU STRING, I\'M DAD')).to eql('nice to meet you string, i\'m dad')
-  end
-end
+# describe '#get_message' do
+#   it 'returns a lowercase message' do
+#     msg = get_message
+#     expect(get_message).to eql(get_message.chomp)
+#   end
+#   it 'returns a lowercase message with non-alphabetic characters' do
+#     expect(get_message('NICE TO MEET YOU STRING, I\'M DAD')).to eql('nice to meet you string, i\'m dad')
+#   end
+# end
 
 describe '#convert_to_array' do
   it 'returns an array of characters' do
-    expect(convert_to_array('hi, friend!')).to eql(['h', 'i', ',', '', 'f', 'r', 'i', 'e', 'n', 'd', '!'])
+    cipher = Cipher.new
+    cipher.msg = 'hi, friend!'
+    cipher.convert_to_array
+    expect(cipher.msg).to eql(['h', 'i', ',', '', 'f', 'r', 'i', 'e', 'n', 'd', '!'])
   end
 end
 
-# Is message punctuation left unencrypted?
-it 'returns an encrypted message where punctuation stays unencrypted' do
-  expect('i\'m dad, son.').to eql('m\'s ifi, xts.')
-end
+# Is message encrypted?
 
-it 'returns an encrypted message where numbers stay unencrypted' do
-  expect('1 great mom').to eql('1 lwjfy rtr')
-end
+# it 'returns a simple letter-only message encrypted with proper shift' do
+#   expect('mom and dad').to eql('rtr tsi ifi')
+# end
 
-it 'returns an encrypted message where non-punctuation, non-numbers, and non-alphabetic characters are unencrypted' do
-  expect('#@mom$~').to eql('#@rtr$~')
-end
+# it 'returns an encrypted message where punctuation stays unencrypted' do
+#   expect('i\'m dad, son.').to eql('m\'s ifi, xts.')
+# end
+
+# it 'returns an encrypted message where numbers stay unencrypted' do
+#   expect('1 great mom').to eql('1 lwjfy rtr')
+# end
+
+# it 'returns an encrypted message where non-punctuation, non-numbers, and non-alphabetic characters are unencrypted' do
+#   expect('#@mom$~').to eql('#@rtr$~')
+# end
 # it 'returns a message that leaves punctuation unencrypted' do
 #   expect(get_message('Wow, what a string!')).to eql('Btb, bmfy f xywnsl!')
 
